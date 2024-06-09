fee293e53f8d59e08bd28522b634762bd1478a90
ViSiOnOp19cr <chandancr98195@email.com>
2024-06-09 07:41:55 -0700

    setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "house",
+    version = "0.0.0",
+    author = "vision",
+    author_email="chandancr98195@gmail.com",
+    packages = find_packages()
+)
